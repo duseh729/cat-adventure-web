6676f49613ad0b25b11f465c5f214ae750d94367
Doyeon <duseh729@naver.com>
2023-09-06 13:54:36 +0900

    feat: 메인 2번째 컨텐츠까지 레이아웃 구현

--- frontend/public/js/contents.js
+++ frontend/public/js/contents.js
@@ -1,13 +1,24 @@
 const firstH1 = document.querySelector(".first-main-contents h1");
 const firstP = document.querySelector(".first-main-contents p");
 
+const secondH1 = document.querySelector(".second-main-contents h1");
+const secondP = document.querySelector(".second-main-contents p");
+
 window.addEventListener("scroll", () => {
-  console.log(document.documentElement.scrollTop);
-  if (document.documentElement.scrollTop > 500) {
+  let scrollTop = document.documentElement.scrollTop;
+  console.log(scrollTop);
+  if (scrollTop > 500) {
     firstH1.classList.add("hide");
     firstP.classList.add("hide");
   } else {
     firstH1.classList.remove("hide");
     firstP.classList.remove("hide");
+  }
+  if (scrollTop < 50 || scrollTop > 1100) {
+    secondH1.classList.add("hide");
+    secondP.classList.add("hide");
+  } else {
+    secondH1.classList.remove("hide");
+    secondP.classList.remove("hide");
   }
 });
