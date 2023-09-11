28044d9225cad7ff6396cb4502aefe296a6c25d8
Doyeon <duseh729@naver.com>
2023-09-11 16:02:50 +0900

    feat: login basic layout develop

--- frontend/public/js/login.js
+++ frontend/public/js/login.js
@@ -1,5 +1,55 @@
 const loginForm = document.getElementById("login-form");
+const signupForm = document.getElementById("signup-form");
 
+const nickname = document.getElementById("nickname");
+const login = document.getElementById("login");
+const signup = document.getElementById("signup");
+const logout = document.getElementById("logout");
+
+const logoutBtn = document.getElementById("logout");
+
+const h1 = document.createElement("h1");
+
+const name = window.localStorage.getItem("name");
+
+// 유저 정보 있을 시 처리
+if (name) {
+  login.style.display = "none";
+  signup.style.display = "none";
+  logout.style.display = "block";
+
+  h1.textContent = `${name}아 반갑다`;
+  nickname.appendChild(h1);
+  // 없을 시 처리
+} else {
+  login.style.display = "inline";
+  signup.style.display = "inline";
+  logout.style.display = "none";
+}
+
+// 로그아웃 처리
+logout.addEventListener("click", () => {
+  window.localStorage.removeItem("name");
+  window.location.href = "/";
+});
+
+//
+login.addEventListener("click", e => {
+  e.preventDefault();
+  signupForm.classList.remove("display-none");
+});
+signup.addEventListener("click", e => {
+  e.preventDefault();
+  signupForm.classList.remove("display-none");
+});
+
+signupForm.addEventListener("click", e => {
+  if (e.target == signupForm) {
+    signupForm.classList.add("display-none");
+  }
+});
+
+// 로그인 인풋값 서버 전송.
 loginForm.addEventListener("submit", e => {
   e.preventDefault();
 
