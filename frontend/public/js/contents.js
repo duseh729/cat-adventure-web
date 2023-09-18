9023a1faa74d111d2f9603590f12fcfc96d23dcd
Doyeon <duseh729@naver.com>
2023-09-18 16:18:53 +0900

    feat: main 페이지 거의 완료 (디자인 수정 필요)

--- frontend/public/js/contents.js
+++ frontend/public/js/contents.js
@@ -4,6 +4,11 @@ const firstP = document.querySelector(".first-main-contents p");
 const secondH1 = document.querySelector(".second-main-contents h1");
 const secondP = document.querySelector(".second-main-contents p");
 
+const thirdH1 = document.querySelectorAll(".third-main-contents-wrapper h1");
+
+const fourthH1 = document.querySelector(".fourth-main-contents-wrapper h1");
+const fourthP = document.querySelector(".fourth-main-contents-wrapper p");
+
 window.addEventListener("scroll", () => {
   let scrollTop = document.documentElement.scrollTop;
   console.log(scrollTop);
@@ -20,5 +25,19 @@ window.addEventListener("scroll", () => {
   } else {
     secondH1.classList.remove("hide");
     secondP.classList.remove("hide");
+  }
+  if (scrollTop < 570) {
+    thirdH1[0].classList.add("hide");
+    thirdH1[1].classList.add("hide");
+  } else {
+    thirdH1[0].classList.remove("hide");
+    thirdH1[1].classList.remove("hide");
+  }
+  if (scrollTop < 710) {
+    fourthH1.classList.add("hide");
+    fourthP.classList.add("hide");
+  } else {
+    fourthH1.classList.remove("hide");
+    fourthP.classList.remove("hide");
   }
 });
