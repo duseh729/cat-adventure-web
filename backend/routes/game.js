2bf4988f88a90f5c221c594ea7e1b4a22e5397d8
Doyeon <duseh729@naver.com>
2023-09-27 18:45:17 +0900

    feat: 게시판 레이아웃 구현

--- backend/routes/game.js
+++ backend/routes/game.js
@@ -6,4 +6,8 @@ router.get("/escape", (req, res) => {
   res.render("game/Escape/index");
 });
 
+router.get("/quiz", (req, res, next) => {
+  res.render("game/Quiz/index");
+});
+
 module.exports = router;
