ec69e5ebe67de86938d191397006e935d49fa171
Doyeon <duseh729@naver.com>
2023-10-19 14:07:44 +0900

    fix: 게임들 주소 추가

--- backend/routes/game.js
+++ backend/routes/game.js
@@ -10,6 +10,16 @@ router.get("/quiz", (req, res, next) => {
   res.render("game/Quiz/index");
 });
 
-router.get("");
+router.get("/tower-defence", (req, res, next) => {
+  res.render("game/Tower-Defence/index");
+});
+
+router.get("/tekken", (req, res, next) => {
+  res.render("game/Tekken/index");
+});
+
+router.get("/packman", (req, res, next) => {
+  res.render("game/Packman/index");
+});
 
 module.exports = router;
